a6c1a169c96914634b521ba432306774213a0013
Luke Przekwas <galactica@The-Galactica.local>
2018-01-31 08:44:27 -0600

    working only if the json file has a [] to start

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+import { format } from "url";
+
 //dependencies
 const express = require("express");
 const path = require("path");
@@ -17,9 +19,41 @@ app.use(bodyParser.urlencoded({ extended: false }))
 //middleware to console log every req.url AND to log it into a txt file in server directory
 app.use((req, res, next) => {
     console.log(req.url);
-    fs.appendFile("./server/url_log.txt", `Logged URL: ${req.url}\n`, (error) => {console.log(error)});
+    fs.appendFile("./server/url_log.txt", `Logged URL: ${req.url}\n`, (error) => { console.log(error) });
     next();
 });
+
+app.post('/tourny-form', (req, res, next) => {
+
+    let rawdata = fs.readFileSync("./server/tournament_log.json");
+    let data = JSON.parse(rawdata);
+
+    let content = {};
+    content['name'] = req.body.name;
+    content['character'] = req.body.character;
+
+    let dataArray = data;
+    dataArray.push(content);
+
+    fs.writeFile("./server/tournament_log.json", JSON.stringify(dataArray), (error) => { console.log(error) });
+
+    res.redirect('/');
+
+    next();
+})
+
+app.get('/formsubmissions', (req, res, next) => {
+    fs.readFile(path.join(__dirname, './tournament_log.json'), {
+        encoding: "UTF-8"
+    }, (err, data) => {
+
+        let formatted = JSON.parse(data);
+        formatted.array.forEach(element => {
+            
+        });
+        res.send(formatted);
+    })
+})
 
 //servers files from /public directory
 app.use(express.static(path.join(__dirname, '../public')));
